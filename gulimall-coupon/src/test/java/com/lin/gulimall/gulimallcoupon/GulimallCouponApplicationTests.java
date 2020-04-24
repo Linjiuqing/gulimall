07b9ee035b5fb1f5c41f315613cb4cfc78dd66f1
Lin <775351884@qq.com>
2020-04-25 01:34:23 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/lin/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
